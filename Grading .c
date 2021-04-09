cf01757f0eebf1cf48d9bfd30e1394e264400b8d
chinnalaa485 <82211301+chinnalaa485@users.noreply.github.com>
2021-04-09 18:38:30 +0530

    Grading.c
    This file displays the grades on screen based on the marks .

---
+++ Grading .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int m;
+Printf("enter the marks",m);
+Scanf("%d",%m);
+if(85=<m=<100)
+Printf("GradeA");
+elif(70=<m=<84)
+Printf("GradeB");
+elif(55=<m=<69)
+Printf("GradeC");
+elif(40=<m=<54)
+Printf("GradeD");
+else
+Printf("GradeF");
+}
